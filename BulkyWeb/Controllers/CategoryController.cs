892c4fb9df1570bf2b2a0edd2c287ef0de2f0df4
TmgGems <gyalwanglama123@gmail.com>
2024-04-18 12:12:18 +0545

    DeletePost Action Created

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -25,11 +25,11 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-            ModelState.AddModelError("Name", "Name and Display order cannot be same .");
+                ModelState.AddModelError("Name", "Name and Display order cannot be same .");
             }
-         
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -41,21 +41,21 @@ namespace BulkyWeb.Controllers
 
         }
 
-        public IActionResult Edit(int ? id)
+        public IActionResult Edit(int? id)
         {
-            if(id == null  )
+            if (id == null)
             {
                 return NotFound();
             }
-            Category ? categoriesFromDb = _db.Categories.Find(id);
+            Category? categoriesFromDb = _db.Categories.Find(id);
             //Category ? categoriesFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
             //Category ? categoriesFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
 
-            if(categoriesFromDb == null)
+            if (categoriesFromDb == null)
             {
                 return NotFound();
             }
-            return View(categoriesFromDb);          
+            return View(categoriesFromDb);
         }
 
         [HttpPost]
@@ -63,11 +63,11 @@ namespace BulkyWeb.Controllers
         public IActionResult Edit(Category obj)
         {
 
-            if(obj.Name  == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("", "Name and DisplayOrder cannot be matched .");
             }
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Update(obj);
                 _db.SaveChanges();
@@ -76,9 +76,41 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
-        public IActionResult Delete()
+        public IActionResult Delete(int? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category? CategoryFromDb = _db.Categories.Find(id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int ? id)
+        {
+            Category? category = _db.Categories.Find(id);
+
+            if(id ==null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Remove(id);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
             return View();
+
+
         }
     }
 }
