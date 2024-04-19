89cc9ae9f33cf71be9f8b0960b5a4e711f5387f5
TmgGems <gyalwanglama123@gmail.com>
2024-04-19 16:25:07 +0545

    Added Repository

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal class IRepository <T> where T : class
+    {
+        //T - category or any other  model that we want to perform CRUD operations
+    }
+}
