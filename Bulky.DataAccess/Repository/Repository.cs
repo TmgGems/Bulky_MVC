b953b9af0cc0380802865497b7ae8de37844a402
TmgGems <gyalwanglama123@gmail.com>
2024-04-21 11:08:20 +0545

    Dbcontext setup of generic class

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.DataAccess.Repository
 {
     public class Repository <T> : IRepository<T> where T : class
     {
+        private readonly ApplicationDbContext _db;
+
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);
         }
 
         public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
