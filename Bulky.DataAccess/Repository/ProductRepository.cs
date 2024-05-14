127c90ad23e88351c0c646fd4322732dd24e830b
Gyalwang Ghhyamjo lama <gyalwanglama123@gmail.com>
2024-05-14 12:21:59 +0545

    Image part done

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -21,7 +21,24 @@ namespace Bulky.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                obj.Price = obj.Price;
+                obj.Price50 = obj.Price50;  
+                obj.ListPrice = obj.ListPrice;
+                obj.Price100 = obj.Price100;
+                obj.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+            
         }
     }
 }
